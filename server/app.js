ccfccadf53780cbc0fb1955808ccb7ae51335144
Harry EMARH <emarh.harry.code@gmail.com>
2025-02-25 13:08:51 +0100

    Setup of our FullStack App

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error(err));
+
+app.get("/", (req, res) => res.send("API Running"));
+
+module.exports = app;
+
+if (require.main === module) {
+    app.listen(5000, () => console.log("Server running on port 5000"));
+}
