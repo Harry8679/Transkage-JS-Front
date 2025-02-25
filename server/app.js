333c3f9c854c95ed5b2b8fe52e5e770efd98b7a4
Harry EMARH <emarh.harry.code@gmail.com>
2025-02-25 18:50:21 +0100

    app updated

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, {})
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error(err));
 
