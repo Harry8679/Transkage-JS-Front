ed56af398fffb017f04808eda536c554f081264c
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-27 22:13:33 +0100

    Token Model 3

--- server/models/token.model.js
+++ server/models/token.model.js
@@ -1,24 +1,10 @@
 const mongoose = require('mongoose');
 
 const tokenSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User',
-  },
-  token: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
-  expiresAt: {
-    type: Date,
-    required: true,
-  },
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  token: { type: String, required: true },
+  createdAt: { type: Date, required: true, default: Date.now },
+  expiresAt: { type: Date, required: true },
 });
 
 const Token = mongoose.model('Token', tokenSchema);
