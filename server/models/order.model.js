bc8adee2bc47cbbf3141bcd4e7d4abc99bba3e79
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-27 22:09:43 +0100

    Order Model 2

--- server/models/order.model.js
+++ server/models/order.model.js
@@ -1,46 +1,16 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  kilos: {
-    type: Number,
-    required: true,
-  },
-  productDescription: {
-    type: String,
-    required: true,
-  },
-  productDimensions: {
-    type: String,
-    required: true,
-  },
-  secretCode: {
-    type: String,
-    required: true,
-  },
-  orderDate: {
-    type: Date,
-    default: Date.now,
-  },
-  isPaid: {
-    type: Boolean,
-    default: false,
-  },
-  pricePerKilo: {
-    type: Number,
-    required: true,
-  },
-  priceTranskage: {
-    type: Number,
-    required: true,
-  },
-  totalPriceTransporter: {
-    type: Number,
-    required: true,
-  },
-  totalPriceTranskage: {
-    type: Number,
-    required: true,
-  },
+  kilos: { type: Number, required: true },
+  productDescription: { type: String, required: true },
+  productDimensions: { type: String, required: true },
+  secretCode: { type: String, required: true },
+  orderDate: { type: Date, default: Date.now },
+  isPaid: { type: Boolean, default: false },
+  pricePerKilo: { type: Number, required: true },
+  priceTranskage: { type: Number, required: true },
+  totalPriceTransporter: { type: Number, required: true },
+  totalPriceTranskage: { type: Number, required: true },
   trackingStatus: { type: String,
     enum: [
       'en attente de remise au transporteur',
