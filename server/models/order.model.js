1e3c36dc8e701ce18912ad7f375f41b0037b0722
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-27 22:07:39 +0100

    Order Model

---
+++ server/models/order.model.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+  kilos: {
+    type: Number,
+    required: true,
+  },
+  productDescription: {
+    type: String,
+    required: true,
+  },
+  productDimensions: {
+    type: String,
+    required: true,
+  },
+  secretCode: {
+    type: String,
+    required: true,
+  },
+  orderDate: {
+    type: Date,
+    default: Date.now,
+  },
+  isPaid: {
+    type: Boolean,
+    default: false,
+  },
+  pricePerKilo: {
+    type: Number,
+    required: true,
+  },
+  priceTranskage: {
+    type: Number,
+    required: true,
+  },
+  totalPriceTransporter: {
+    type: Number,
+    required: true,
+  },
+  totalPriceTranskage: {
+    type: Number,
+    required: true,
+  },
+  trackingStatus: { type: String,
+    enum: [
+      'en attente de remise au transporteur',
+      'commande validée par le transporteur',
+      'remis au transporteur',
+      'remis au destinataire',
+      'refusé',
+    ], 
+    default: 'en attente de remise au transporteur' },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  transporter: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  trip: { type: mongoose.Schema.Types.ObjectId, ref: 'Trip', required: true },
+  validateByTransporter: { type: Boolean, default: false }
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
