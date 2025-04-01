35c4281f3b7fe8f56017f44647eb8bf975151669
Harry MacCode <emarh.harry.code@gmail.com>
2025-04-01 06:58:10 +0200

    Import to require 6

--- server/routes/trip.route.js
+++ server/routes/trip.route.js
@@ -1,25 +1,23 @@
-// trip.route.ts
+const express = require('express');
+const {
+  createTrip,
+  getTripById,
+  getTrips,
+  getUserTrips,
+  updateTrip
+} = require('../controllers/trip.controller');
 
-import { Router } from 'express';
-import { createTrip, getTripById, getTrips, getUserTrips, updateTrip } from '../controllers/trip.controller';
-import { adminMiddleware, protect } from '../middlewares/auth.middleware';
-// import { createTrip } from './trip.controller';
+const {
+  protect,
+  adminMiddleware
+} = require('../middlewares/auth.middleware');
 
-const router = Router();
+const router = express.Router();
 
-// Route POST /api/v1/trips pour créer un trajet
 router.post('/', protect, createTrip);
-
-// Route GET /api/v1/trips pour récupérer tous les trajets
 router.get('/', getTrips);
-
-// Route GET /api/v1/trips/mine pour récupérer les trajets de l'utilisateur connecté
 router.get('/mine', protect, getUserTrips);
-
-// Route PUT /api/v1/trips/:id pour modifier un trajet
 router.put('/:id', protect, adminMiddleware, updateTrip);
+router.get('/:id', getTripById);
 
-router.get('/:id', getTripById); // Route pour récupérer un trajet par son ID
-
-
-export default router;
+module.exports = router;
