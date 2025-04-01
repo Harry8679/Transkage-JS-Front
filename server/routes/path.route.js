61196c37e7a8bacfa748288f65d94978359f5bc9
Harry MacCode <emarh.harry.code@gmail.com>
2025-04-01 06:57:18 +0200

    Import to require 4

--- server/routes/path.route.js
+++ server/routes/path.route.js
@@ -1,12 +1,9 @@
-import express from 'express';
-import { createPath, getAllPaths } from '../controllers/path.controller';
+const express = require('express');
+const { createPath, getAllPaths } = require('../controllers/path.controller');
 
 const router = express.Router();
 
-// Route pour créer un nouveau trajet
 router.post('/paths', createPath);
-
-// Route pour obtenir tous les trajets
 router.get('/paths', getAllPaths);
 
-export default router;
+module.exports = router;
